5c2e70ec3503508ba81118ead9094d4815224edb
SaurabhT311 <tripathisaurabh.311@gmail.com>
2020-12-11 01:42:18 +0530

    compute a%b+c

--- arithmeticsort.sh
+++ arithmeticsort.sh
@@ -8,3 +8,4 @@ read -p "Enter a Third Number : " c
 val1=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a+b*c} END {print compute}'`
 val2=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a*b+c} END {print compute}'`
 val3=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=c+a/b} END {print compute}'`
+val4=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a%b+c} END {print compute}'`
