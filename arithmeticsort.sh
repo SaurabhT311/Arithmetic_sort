5303992752640d928e73f1c47c595c15488c17c4
SaurabhT311 <tripathisaurabh.311@gmail.com>
2020-12-11 01:57:03 +0530

    Storing the result in Dictionary

--- arithmeticsort.sh
+++ arithmeticsort.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash -x
+
+usr/bin/bash -x
 echo " welcome to compute and sort arithematic numbers "
+
+declare -A dict
 
 read -p "Enter First Number : " a
 read -p "Enter Second Number : " b
 read -p "Enter a Third Number : " c
 
-val1=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a+b*c} END {print compute}'`
-val2=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a*b+c} END {print compute}'`
-val3=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=c+a/b} END {print compute}'`
-val4=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a%b+c} END {print compute}'`
+dict[1]=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a+b*c} END {print compute}'`
+dict[2]=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a*b+c} END {print compute}'`
+dict[3]=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=c+a/b} END {print compute}'`
+dict[4]=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a%b+c} END {print compute}'`
