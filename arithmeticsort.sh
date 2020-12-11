9831bcc10085e63d1e5c1937d00e274614b9301d
SaurabhT311 <tripathisaurabh.311@gmail.com>
2020-12-11 19:19:54 +0530

    Taken 3 input number

--- arithmeticsort.sh
+++ arithmeticsort.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/bash -x
 
-usr/bin/bash -x
 echo " welcome to compute and sort arithematic numbers "
 
-declare -A dict
 
 read -p "Enter First Number : " a
 read -p "Enter Second Number : " b
 read -p "Enter a Third Number : " c
 
-dict[1]=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a+b*c} END {print compute}'`
-dict[2]=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a*b+c} END {print compute}'`
-dict[3]=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=c+a/b} END {print compute}'`
-dict[4]=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a%b+c} END {print compute}'`
